6a82a7d10017becbf6fd943b9339f92f872458b4
CornerstoneProductsGroup <gfetzer@4cct.com>
2026-02-02 10:19:46 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 
+MONTH_NAME_TO_NUM = {
+    "January": 1, "February": 2, "March": 3, "April": 4,
+    "May": 5, "June": 6, "July": 7, "August": 8,
+    "September": 9, "October": 10, "November": 11, "December": 12
+}
+
+
 import pandas as pd
 def avg_ignore_zeros_cols(row, cols):
     """
